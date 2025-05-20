c71881187bd564206627a5cdfc63bb2f93955d16
Mowassir Noor <mowassirnoor05@gmail.com>
2025-05-21 02:25:30 +0300

    cors correction

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // CORS middleware
 app.use(cors({
   origin: 'http://127.0.0.1:3000', // or 'http://localhost:3000'
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   credentials: true
 }));
 
