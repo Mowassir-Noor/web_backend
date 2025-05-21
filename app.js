3538e529e2863147917f544b89db17cb57787c32
Mowassir Noor <mowassirnoor05@gmail.com>
2025-05-21 05:09:08 +0300

    depl

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 // CORS middleware
 app.use(cors({
-  origin: 'http://127.0.0.1:3000', // or 'http://localhost:3000'
+  origin: 'https://web-dev-project-3vhy.onrender.com', // or 'http://localhost:3000'
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   credentials: true
 }));
